18571fe3d8c959c2550d3e6d49e1c792976d11da
Balaprasad133 <balaprasadh133@gmail.com>
2023-12-12 11:21:54 +0530

    new demo file is updated

---
+++ src/Demo1.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+function Demo1() {
+  
+  return (
+      <div className="menu-data">
+        <h1>Welcome to demo file</h1>
+      </div>
+  );
+}
+
+export default Demo1;
